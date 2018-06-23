4bca1cbbddcaae4681e3a3a2ec4c8e96efffe57f
neor1x <33566720+neor1x@users.noreply.github.com>
2018-06-23 18:34:55 +0800

    Update and rename redfoxv01.js to redfoxv02.js

--- redfoxv02.js
+++ redfoxv02.js
@@ -16907,7 +16907,7 @@
                     className: "comp-anime-list"
                 }, t && y.default.createElement("div", {
                     className: "list-title"
-                }, v.default.createElement(null, this.renderIcon(y.default.three-bars.toSVG()), "\u0410\u043D\u0438\u043C\u044D"), y.default.createElement(k.default, i, t.map(function(e, t) {
+                }, v.default.createElement(y.default.three-bars.toSVG()), "\u0410\u043D\u0438\u043C\u044D"), y.default.createElement(k.default, i, t.map(function(e, t) {
                     var n = e.content,
                         r = e.nextPageToken,
                         o = e.label;
